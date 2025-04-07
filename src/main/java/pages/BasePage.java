d8b580737d04845cf8a81290ab2ee465d4c62992
Olesya Kolenchenko <kololesya@gmail.com>
2025-04-07 13:31:44 +0200

    refactoring BasePage

--- src/main/java/pages/BasePage.java
+++ src/main/java/pages/BasePage.java
@@ -6,21 +6,18 @@ import org.openqa.selenium.WebDriver;
 import java.util.Optional;
 
 public class BasePage extends AbstractPage {
+
     public BasePage(WebDriver driver) {
         super(driver);
     }
 
     public void open() {
-        open(null);
+        open(getUrl().orElseThrow(() ->
+                new IllegalStateException("URL must not be null. Either override getUrl() or pass a URL to open.")));
     }
 
     public void open(String url) {
-        String targetUrl = (url != null) ? url : getUrl().orElse(null);
-        if (targetUrl != null) {
-            getDriver().get(targetUrl);
-        } else {
-            throw new IllegalStateException("URL must not be null. Either override getUrl() or pass a URL to open().");
-        }
+        getDriver().get(url); // Открываем URL
     }
 
     public Optional<String> getUrl() {
